dd88e9fcaad3d61edbcb0b97c4d78bfe8af39085
James Heggie <>
2021-11-18 21:27:58 +0000

    Mobile stuff

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -26,43 +26,39 @@ requirejs(['gsap', 'gsap.ScrollTrigger'], function(Greensock, ScrollTriggerNS) {
     const newNoteNode = document.createElement('div');
     const newNoteMarker = document.createElement('div');
     const newNoteContent = document.querySelector('.main-viewport .note-list .'+noteName+' .content').cloneNode(true);
+    const needEmptyP = !(noteLocation.previousElementSibling instanceof HTMLParagraphElement);
 
     newNoteMarker.className = 'marker';
     newNoteNode.className = 'note';
 
-    newNoteMarker.innerText = 'N';
+    newNoteMarker.innerHTML = '<i class="fas fa-angle-double-right"></i>';
 
     newNoteNode.appendChild(newNoteMarker);
     newNoteNode.appendChild(newNoteContent);
 
     noteLocation.parentNode.insertBefore(newNoteNode, noteLocation);
 
+    if(needEmptyP) {
+      noteLocation.parentNode.insertBefore(
+        document.createElement('p'), newNoteNode);
+    }
+
     ScrollTrigger.create({
       trigger: note,
-      onEnter: (e) => { ShowNote(e.trigger); },
-      onEnterBack: (e) => { ShowNote(e.trigger); },
-      onLeave: (e) => { HideNote(e.trigger); },
-      onLeaveBack: (e) => { HideNote(e.trigger); },
+      onEnter: (e) => { ShowNoteLink(e.trigger); },
+      onEnterBack: (e) => { ShowNoteLink(e.trigger); },
+      onLeave: (e) => { HideNoteLink(e.trigger); },
+      onLeaveBack: (e) => { HideNoteLink(e.trigger); },
     });
 
   });
 
+  document.querySelector('.main-viewport .content').addEventListener('click', (e) => {
+    ShowNote(e);
+  });
+
   document.querySelector('.note-list').addEventListener('click', (e) => {
-    document.body.classList.add('showing-note');
-
-    const noteDisplay = document.querySelector('.notes .note');
-
-    noteDisplay.textContent = '';
-
-    const closeButton = document.createElement('div');
-    closeButton.classList.add('close-button');
-    closeButton.innerText = '[close]';
-
-    noteDisplay.appendChild(closeButton);
-    noteDisplay.appendChild(
-      e.target.closest('.note').querySelector('.content').cloneNode(true)
-    );
-
+    ShowNote(e);
   });
 
   document.querySelector('.notes .note').addEventListener('click', (e) => {
@@ -78,11 +74,33 @@ requirejs(['gsap', 'gsap.ScrollTrigger'], function(Greensock, ScrollTriggerNS) {
   });
 
   function ShowNote(e) {
+
+    if (!e.target.closest('.note')) {
+      return;
+    }
+
+    document.body.classList.add('showing-note');
+
+    const noteDisplay = document.querySelector('.notes .note');
+
+    noteDisplay.textContent = '';
+
+    const closeButton = document.createElement('div');
+    closeButton.classList.add('close-button');
+    closeButton.innerText = '[close]';
+
+    noteDisplay.appendChild(closeButton);
+    noteDisplay.appendChild(
+      e.target.closest('.note').querySelector('.content').cloneNode(true)
+    );
+  }
+
+  function ShowNoteLink(e) {
     const id = e.attributes.getNamedItem('note').value;
     document.querySelector('.main-viewport .note-list .'+id+' .title').style.display = 'block';
   }
 
-  function HideNote(e) {
+  function HideNoteLink(e) {
     const id = e.attributes.getNamedItem('note').value;
     document.querySelector('.main-viewport .note-list .'+id+' .title').style.display = 'none';
   }
